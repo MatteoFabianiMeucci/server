2e88f7c91cfe40080dbe6393abf9b475ab6b48a0
lai2-24 <lai2-24@itismeucci.com>
2024-10-15 10:00:10 +0200

    Aggiunta funzionalità e gestione con protocollo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,9 +1,5 @@
 package com.example;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
--- src/main/java/com/example/MioThread.java
+++ src/main/java/com/example/MioThread.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+import javax.print.DocFlavor.STRING;
+
 public class MioThread extends Thread{
     Socket s;
 
@@ -18,19 +20,56 @@ public class MioThread extends Thread{
             DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
             String stringRicevuta;
+            String stringInviata;
+            boolean acceso = true;
+
             do{
                 stringRicevuta = in.readLine();
                 
-                if(!stringRicevuta.equals("!")){
-                    System.out.println("La stringa ricevuta: " + stringRicevuta);
-                    String stringMaiuscola = stringRicevuta.toUpperCase();
-                    out.writeBytes(stringMaiuscola + "\n");
-                }
-            } while(!stringRicevuta.equals("!"));
+                switch (stringRicevuta) {
+                    case "M":
+                        out.writeBytes("?" + "\n");
+                        stringInviata = in.readLine().toUpperCase();
+                        out.writeBytes(stringInviata + "\n");
+                        break;
 
+                    case "m":
+                        out.writeBytes("?" + "\n");
+                        stringInviata = in.readLine().toLowerCase();
+                        out.writeBytes(stringInviata + "\n");
+                        break;
+
+                    case "r":
+                        out.writeBytes("?" + "\n");
+                        stringInviata = in.readLine();
+
+                        StringBuilder reverse = new StringBuilder();
+                        reverse.append(stringInviata);
+                        reverse.reverse();
+                        stringInviata = reverse.toString();
+
+                        out.writeBytes(stringInviata + "\n");
+                        break;
+
+                    case "c":
+                        out.writeBytes("?" + "\n");
+                        int conto = in.readLine().length();
+                        stringInviata = String.valueOf(conto);
+                        out.writeBytes(stringInviata + "\n");
+
+                    case "!":
+                        acceso = false;
+                        break;
+
+
+                    default:
+                        out.writeBytes("#" + "\n");
+                        break;
+                }
+            } while(acceso);
             s.close();
+            
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         
